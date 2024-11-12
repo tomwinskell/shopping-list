1e8ae2af2cb22dd7862cca240d234accfadb1683
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-11-12 12:53:03 -0500

    Use fetch for post and delete

--- public/app.js
+++ public/app.js
@@ -1,31 +1,64 @@
 // Assign variables to html elements
-const ul = document.querySelector("ul");
+const deleteButtons = document.querySelectorAll("button[id^=js__delete]");
 const input = document.querySelector("input");
-const addButton = document.querySelector("button");
+const addButton = document.querySelector("button[id^=js__add]");
+const ul = document.querySelector("ul");
 
 // Add click event to button, calls add item function
-// addButton.addEventListener('click', () => addItem());
+addButton.addEventListener('click', () => addElement(input.value));
 
 // Add keypress event to input
 input.addEventListener('keypress', (e) => {
   if (e.key === "Enter") {
     addButton.click();
-  }
-})
+  };
+});
+
+// Add delete button functionality
+deleteButtons.forEach(btn => {
+  btn.addEventListener('click', event => deleteElement(event));
+});
+
+const deleteElement = (event) => {
+  const item = event.target.parentNode;
+  fetchItem(item.querySelector('span').textContent, '/', 'delete');
+  item.remove();
+};
+
+// Make request to backend
+const fetchItem = async (item, url, method) => {
+  let response;
+  try {
+    response = await fetch(url, {
+      method: method,
+      body: JSON.stringify(item)
+    });
+  } catch (error) {
+    console.error(error.message);
+  };
+  if (response?.ok) {
+    console.log(`${method} ${item}`);
+  } else {
+    throw new Error (`Response status: ${response.status}`);
+  };
+};
 
 // Add new shopping list item
-function addItem() {
+const addElement = (item) => {
+  fetchItem(item, '/', 'post');
 
   // Create new list item using input value
+  const span = document.createElement('span');
   const li = document.createElement('li');
-  li.textContent = input.value;
+  span.textContent = input.value;
 
   input.value = "";
 
   // Create span and delete button
-  const span = document.createElement('span');
   const deleteButton = document.createElement('button');
   deleteButton.textContent = "Delete";
+  deleteButton.id = "js__delete-button";
+  deleteButton.setAttribute("type", "button"); 
   
   // Append new elements to ul
   li.appendChild(span);
@@ -33,14 +66,9 @@ function addItem() {
   ul.appendChild(li);
 
   // Add event listener to delete button
-  deleteButton.addEventListener('click', () => removeItem(li));
+  deleteButton.addEventListener('click', event => deleteElement(event));
 
   // Return cursor to input
   input.focus();
 }
 
-// Delete item from shopping list
-function removeItem(li) {
-  li.remove();
-  input.focus();
-}
--- server.js
+++ server.js
@@ -11,70 +11,77 @@ const items = ['pineapple', 'apple', 'bread'];
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Route handler for POST /
-const addItem = (req, res) => {
+// route handler for 'post' and 'delete'
+const handleItem = (req, res, callback) => {
   let body = '';
   req.on('data', (chunk) => {
     body += chunk;
-  });
+  })
   req.on('end', () => {
-    const newItem = qs.parse(body);
-    items.push(newItem['item']);
+    const data = JSON.parse(body)
+    callback(data);
     res.end();
-  });
-}
+  })
+};
 
-// Route handler for GET /
+// Route handler for 'get'
 const displayList = async (req, res) => {
-  
   const filePath = path.join(__dirname, 'views', 'template.hbs');
   const data = await fs.readFile(filePath);
   const source = data.toString();
-
-  // const source = `<ul>{{#each this}}
-  // <li>{{this}}<span><button>Delete</button></span></li>
-  // {{/each}}</ul>`;
   const template = Handlebars.compile(source);
   const output = template(items);
-  console.log(output);
   res.setHeader('content-type', 'text/html');
   res.write(output);
   res.end();
 }
 
+// Handle static files
+const staticFileHandler = async (req, res, url) => {
+  const filePath = path.join(__dirname, url);
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+  };
+  const extname = String(path.extname(url)).toLowerCase();
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  const data = await fs.readFile(filePath);
+  res.writeHead(200, {'content-type': contentType});
+  res.end(data)
+};
+
 const server = createServer( async (req, res) => {
   try {
-    
-    if (req.method === 'GET') {
-      let filePath;
-      if (req.url === '/') {
+    const pattern = '^\/public\/[a-zA-Z]+.[a-zA-Z]+';
+    const { url } = req;
+
+    if (url === '/') {
+      if (req.method === 'GET') {
         displayList(req, res);
-      } else if (req.url === '/app.js') {
-        filePath = path.join(__dirname, 'public', 'app.js');
-      } else {
-        res.writeHead(404, {'content-type': 'text/html'});
-        res.end('<h1>Page not found</h1>');
-        return;
-      }
-      // const data = await fs.readFile(filePath);
-      // res.setHeader('content-type', 'text/html');
-      // res.write(data);
-      // res.end();
-    } 
-    
-    else if (req.method === 'POST') {
-      if (req.url === '/') {
-        addItem(req, res);
+      } else if (req.method === 'POST') {
+        // handle JSON then add to array
+        handleItem(req, res, (data) => {
+          items.push(data);
+        });
+      } else if (req.method === 'DELETE') {
+        // handle JSON then delete from array
+        handleItem(req, res, (data) => {
+          const index = items.indexOf(data);
+          items.splice(index, 1);
+        });
       } else {
         res.writeHead(403, {'content-type': 'text/html'});
         res.end('<h1>Request not allowed</h1>');
         return;
       }
-    }
-    
-    else {
-      res.writeHead(403, {'content-type': 'text/html'});
-      res.end('<h1>Request not allowed</h1>');
+    } else if (url.match(pattern)) {
+      staticFileHandler(req, res, url);
+    } else {
+      res.writeHead(404, {'content-type': 'text/html'});
+      res.end('<h1>Page not found</h1>');
+      return;
     }
   } catch (error) {
     console.log(error);
